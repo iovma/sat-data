68438dfc4c3732ad3ec47a4fd350b47a6562c210
Gnlow <gnlow@protonmail.com>
2023-08-02 03:34:32 +0900

    tsv

---
+++ script/jsonToTsv.ts
@@ -0,0 +1,19 @@
+import { cats } from "./cats.ts"
+
+await Promise.all(Object.entries(cats).map(async function([cat]) {
+    const table = JSON.parse(await Deno.readTextFile(`dist/${cat}.json`))
+    const columns = new Set<string>()
+    
+    table.forEach((row: Record<string, string>) => {
+        Object.keys(row).forEach(x=>columns.add(x))
+    })
+    const columnsArray = Array.from(columns)
+
+    const result =
+        columnsArray.join("\t") + "\n" +
+        table.map((row: Record<string, string>) =>
+            columnsArray.map(x=>row[x] || "").join("\t")
+        ).join("\n")
+
+    await Deno.writeTextFile(`dist/${cat}.tsv`, result)
+}))
