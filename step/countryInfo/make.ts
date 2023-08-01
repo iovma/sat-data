bbfd3305cf956c68f4b55211aeec0b08da03352e
Gnlow <gnlow@protonmail.com>
2023-08-02 02:02:28 +0900

    delete /step

--- step/countryInfo/make.ts
+++
@@ -1,8 +0,0 @@
-import { parseCountryInfo } from "../../src/parseCountryInfo.ts"
-
-const columns = (await Deno.readTextFile("columns.txt")).split("\n").map(x=>x.trim())
-console.log(columns)
-
-const file = await Deno.readTextFile("countryInfo.xml")
-const data = parseCountryInfo(file)
-Deno.writeTextFile( "countryInfo.json", JSON.stringify(data))
